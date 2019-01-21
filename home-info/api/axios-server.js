80ddafb035fac1c99a9e6bd9fcd68270224a5e5a
garryk3 <egoruum@gmail.com>
2019-01-22 01:32:25 +0300

    add rest server (авторизация)

--- home-info/api/axios-server.js
+++ home-info/api/axios-server.js
@@ -3,7 +3,10 @@ import config from './config';
 
 const instance = axios.create({
 	baseURL: config.domoticzUrl,
-	timeout: config.timeouts.server
+	timeout: config.timeouts.server,
+	headers: {
+		'Authorization': 'Basic Z2FycnlrMzpnYWdhcmExOTgy'
+	}
 });
 
 export default instance;
--- home-info/api/v1/auth.js
+++ home-info/api/v1/auth.js
@@ -1,28 +1,50 @@
 import express from 'express';
-import qs from 'qs';
-
 import axios from '../axios-server';
 
 const router = express.Router();
 
 export default () => {
+	const admin = {
+		user: 'test',
+		password: 'test'
+	};
+
 	router.route('/')
 		.post((req, res) => {
 			const params = {
 				type: 'command',
-				param: 'logincheck',
-				username: 'Z2FycnlrMw==',
-				password: '63ce93b2b78e041d2869176e38ce8569',
-				rememberme: false
+				param: 'getauth'
 			};
 
 			if(req.query) {
-				axios.post('/json.htm', qs.stringify(params))
-					.then((res) => {
-						res.send(res)
+				axios.get('/json.htm', { params })
+					.then((response) => {
+						const info = response.data;
+
+						if((response.status === 200) && info) {
+							if(info.rights === 2 && info.status === 'OK') {
+								res.send({
+									code: 200,
+									message: 'success'
+								})
+							} else {
+								res.send({
+									code: 500,
+									message: `Авторизация не удалась, недостаточно прав (${info.rights}-${info.status}-${info.statusText}) `
+								})
+							}
+						} else {
+							res.send({
+								code: 500,
+								message: `Авторизация не удалась, ошибка сервера, статус: ${response.status}`
+							})
+						}
 					})
 					.catch((error) => {
-						res.send(error.response.data);
+						res.send({
+							code: 500,
+							message: `Ошибка обработки запроса: ${error.message}`
+						});
 						console.error(error);
 					})
 			}
