18b76b7121a6693e367efffd40d9b48c74af63aa
garryk3 <egoruum@gmail.com>
2019-01-30 01:28:28 +0300

    server (basic auth)

--- home-info/api/v1/index.js
+++ home-info/api/v1/index.js
@@ -1,23 +1,13 @@
 import express from 'express';
-import basicAuth from 'express-basic-auth';
-
-import users from './../config/users';
 
 import WaterHeater from './waterheater';
 import System from './system';
 import Light from './light';
 
 const router = express.Router();
-const checkAuth = basicAuth({
-	users,
-	unauthorizedResponse: {
-		code: 401,
-		message: 'Пользователь не авторизован!'
-	}
-});
 
-router.use('/waterheater', checkAuth, WaterHeater());
-router.use('/system', checkAuth, System());
-router.use('/light', checkAuth, Light());
+router.use('/waterheater', WaterHeater());
+router.use('/system', System());
+router.use('/light', Light());
 
 export default router
